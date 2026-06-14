4b00b46115a7a1b5c885239f2a4c3a07d61ace55
ALpht <skewed12358@gmail.com>
2026-06-14 22:24:31 +0800

    feat(turn-manager): add TurnManager with turn rotation and TURN_CHANGED events

--- src/state/TurnManager.js
+++ src/state/TurnManager.js
@@ -1,3 +1,9 @@
+import { EventBus }
+    from '../core/EventBus.js';
+
+import { EventTypes }
+    from '../core/EventTypes.js';
+
 export class TurnManager {
 
     #players;
@@ -16,4 +22,115 @@ export class TurnManager {
 
     }
 
+    start(players) {
+
+        if (!players?.length) {
+
+            throw new Error(
+                'TurnManager requires players'
+            );
+
+        }
+
+        this.#players = players;
+
+        this.#currentIndex = 0;
+
+        this.#started = true;
+
+        this.#emitTurnChanged();
+
+    }
+
+    hasStarted() {
+
+        return this.#started;
+
+    }
+
+    getCurrentPlayer() {
+
+        if (!this.#started) {
+
+            return null;
+
+        }
+
+        return this.#players[
+            this.#currentIndex
+        ];
+
+    }
+
+    getCurrentPlayerIndex() {
+
+        if (!this.#started) {
+
+            return -1;
+
+        }
+
+        return this.#currentIndex;
+
+    }
+
+    nextTurn() {
+
+        if (!this.#started) {
+
+            throw new Error(
+                'TurnManager has not started'
+            );
+
+        }
+
+        this.#currentIndex =
+            (
+                this.#currentIndex + 1
+            )
+            %
+            this.#players.length;
+
+            this.#emitTurnChanged();
+
+    }
+
+    isCurrentPlayer(player) {
+
+        if (!this.#started) {
+
+            return false;
+
+        }
+
+        return (
+            this.getCurrentPlayer().id
+            ===
+            player.id
+        );
+
+    }
+
+    #emitTurnChanged() {
+
+        const player =
+            this.getCurrentPlayer();
+
+        EventBus.emit(
+
+            EventTypes.TURN_CHANGED,
+
+            {
+
+                currentPlayerId:
+                    player.id,
+
+                turnIndex:
+                    this.#currentIndex
+
+            }
+
+        );
+
+    }
 }
