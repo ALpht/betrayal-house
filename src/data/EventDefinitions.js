98f07ab523a47985d5f6ff0ae364aa2d50d07b85
ALpht <skewed12358@gmail.com>
2026-06-17 21:06:38 +0800

    feat(card-data): add event definitions
    Add 10 event card definitions with semantic IDs:
    - Burning Man, Secret Passage, Spider Webs, Closet Door, Footsteps
    - Creepy Puppet, Strange Noise, Cold Wind, Vanishing Room, Broken Clock
    
    Each card includes: id, expansion, type, name, description, tags, rarity

---
+++ src/data/EventDefinitions.js
@@ -0,0 +1,103 @@
+export const EventDefinitions = [
+
+    {
+        id: 'event_burning_man',
+        expansion: 'base',
+        type: 'event',
+        name: 'Burning Man',
+        description: 'A horribly burned man staggers toward you.',
+        tags: ['damage'],
+        rarity: 'common'
+    },
+
+    {
+        id: 'event_secret_passage',
+        expansion: 'base',
+        type: 'event',
+        name: 'Secret Passage',
+        description: 'You discover a hidden passage behind the bookshelf.',
+        tags: ['movement'],
+        rarity: 'common'
+    },
+
+    {
+        id: 'event_spider_webs',
+        expansion: 'base',
+        type: 'event',
+        name: 'Spider Webs',
+        description: 'Thick spider webs block your path.',
+        tags: ['obstacle'],
+        rarity: 'common'
+    },
+
+    {
+        id: 'event_closet_door',
+        expansion: 'base',
+        type: 'event',
+        name: 'Closet Door',
+        description: 'A closet door creaks open by itself.',
+        tags: ['exploration'],
+        rarity: 'common'
+    },
+
+    {
+        id: 'event_footsteps',
+        expansion: 'base',
+        type: 'event',
+        name: 'Footsteps',
+        description: 'You hear footsteps behind you, but no one is there.',
+        tags: ['psychological'],
+        rarity: 'common'
+    },
+
+    {
+        id: 'event_creepy_puppet',
+        expansion: 'base',
+        type: 'event',
+        name: 'Creepy Puppet',
+        description: 'A doll on the shelf turns its head to watch you.',
+        tags: ['psychological'],
+        rarity: 'uncommon'
+    },
+
+    {
+        id: 'event_strange_noise',
+        expansion: 'base',
+        type: 'event',
+        name: 'Strange Noise',
+        description: 'A strange noise echoes from somewhere in the house.',
+        tags: ['exploration'],
+        rarity: 'common'
+    },
+
+    {
+        id: 'event_cold_wind',
+        expansion: 'base',
+        type: 'event',
+        name: 'Cold Wind',
+        description: 'A freezing cold wind blows through the room, extinguishing your light.',
+        tags: ['obstacle'],
+        rarity: 'common'
+    },
+
+    {
+        id: 'event_vanishing_room',
+        expansion: 'base',
+        type: 'event',
+        name: 'Vanishing Room',
+        description: 'The room behind you has disappeared. There is no way back.',
+        tags: ['movement', 'obstacle'],
+        rarity: 'uncommon'
+    },
+
+    {
+        id: 'event_broken_clock',
+        expansion: 'base',
+        type: 'event',
+        name: 'Broken Clock',
+        description: 'All the clocks in the house have stopped at the same time.',
+        tags: ['psychological'],
+        rarity: 'common'
+    }
+
+];
