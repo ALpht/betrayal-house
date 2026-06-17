ed9c747153bf14f9be2195f4fc4b37f648648338
ALpht <skewed12358@gmail.com>
2026-06-17 21:11:39 +0800

    feat(card-data): add item definitions
    Add 5 item card definitions with semantic IDs:
    - Axe, Armor, Healing Salve, Dynamite, Medical Kit
    
    Each card includes: id, expansion, type, name, description, tags, rarity

---
+++ src/data/ItemDefinitions.js
@@ -0,0 +1,53 @@
+export const ItemDefinitions = [
+
+    {
+        id: 'item_axe',
+        expansion: 'base',
+        type: 'item',
+        name: 'Axe',
+        description: 'A dangerous weapon.',
+        tags: ['weapon'],
+        rarity: 'common'
+    },
+
+    {
+        id: 'item_armor',
+        expansion: 'base',
+        type: 'item',
+        name: 'Armor',
+        description: 'A suit of armor that provides protection.',
+        tags: ['protection'],
+        rarity: 'common'
+    },
+
+    {
+        id: 'item_healing_salve',
+        expansion: 'base',
+        type: 'item',
+        name: 'Healing Salve',
+        description: 'A soothing salve that heals wounds.',
+        tags: ['healing'],
+        rarity: 'common'
+    },
+
+    {
+        id: 'item_dynamite',
+        expansion: 'base',
+        type: 'item',
+        name: 'Dynamite',
+        description: 'A stick of dynamite. Handle with care.',
+        tags: ['weapon', 'destruction'],
+        rarity: 'uncommon'
+    },
+
+    {
+        id: 'item_medical_kit',
+        expansion: 'base',
+        type: 'item',
+        name: 'Medical Kit',
+        description: 'A compact medical kit for emergencies.',
+        tags: ['healing'],
+        rarity: 'common'
+    }
+
+];
