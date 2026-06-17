338de1e588e4ee7175856c0868b96e658514b715
ALpht <skewed12358@gmail.com>
2026-06-17 21:11:42 +0800

    feat(card-data): add omen definitions
    Add 5 omen card definitions with semantic IDs:
    - Skull, Madman, Dog, Ring, Book
    
    Each card includes: id, expansion, type, name, description, tags, rarity

---
+++ src/data/OmenDefinitions.js
@@ -0,0 +1,53 @@
+export const OmenDefinitions = [
+
+    {
+        id: 'omen_skull',
+        expansion: 'base',
+        type: 'omen',
+        name: 'Skull',
+        description: 'A skull with strange power.',
+        tags: ['omen'],
+        rarity: 'common'
+    },
+
+    {
+        id: 'omen_madman',
+        expansion: 'base',
+        type: 'omen',
+        name: 'Madman',
+        description: 'A figure with wild eyes who speaks in riddles.',
+        tags: ['omen', 'psychological'],
+        rarity: 'common'
+    },
+
+    {
+        id: 'omen_dog',
+        expansion: 'base',
+        type: 'omen',
+        name: 'Dog',
+        description: 'A loyal dog that follows you silently.',
+        tags: ['omen'],
+        rarity: 'common'
+    },
+
+    {
+        id: 'omen_ring',
+        expansion: 'base',
+        type: 'omen',
+        name: 'Ring',
+        description: 'A golden ring engraved with ancient symbols.',
+        tags: ['omen'],
+        rarity: 'uncommon'
+    },
+
+    {
+        id: 'omen_book',
+        expansion: 'base',
+        type: 'omen',
+        name: 'Book',
+        description: 'A dusty tome written in an unknown language.',
+        tags: ['omen', 'knowledge'],
+        rarity: 'common'
+    }
+
+];
